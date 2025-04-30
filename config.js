4d5f5791f9576b77eb97a13ddd778ffe80bab9e2
aconstas <constasboy@gmail.com>
2025-04-29 22:08:36 -0700

    create config file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    kafka: {
+      brokers: process.env.KAFKA_BROKERS?.split(',') || ["localhost:9092"],
+      topics: {
+        weatherData: "weather-data-events",
+        processedData: "processed-data"
+      }
+    }
+  };
