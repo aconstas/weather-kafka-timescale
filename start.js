679bcf481240aebd865cbca6457d675e0805b8de
aconstas <constasboy@gmail.com>
2025-04-29 22:09:57 -0700

    add async startup function

--- start.js
+++ start.js
@@ -1,6 +1,21 @@
-const dataGenerator = require('./services/data-generator');
-const dataProcessor = require('./services/data-processor')
+const dataGenerator = require("./services/data-generator");
+const dataProcessor = require("./services/data-processor");
 
+console.log("Starting Weather Kafka Application...");
 
-dataGenerator.connect();
-dataProcessor.startDataProcessor();
+async function startApplication() {
+  try {
+    console.log("Initializing Data Generator...");
+    await dataGenerator.connect();
+
+    console.log("Initializing Data Processor...");
+    await dataProcessor.startDataProcessor();
+
+    console.log("All services started successfully!");
+  } catch (error) {
+    console.error("Failed to start application:", error);
+    process.exit(1);
+  }
+}
+
+startApplication();
